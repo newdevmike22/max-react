04deaca420ed0d285727ce10a0469b4591aae0fb
MLDODSON <paola@dominicansoul.com>
2023-12-09 19:14:47 -0600

    Dynamically loading image files
    App update. Dynamically loading image files.

--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,3 +1,6 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+
 const reactDiscriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +12,7 @@ const Header = () => {
 
     return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="" />
+        <img src={reactImg} alt="Stylized Atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
